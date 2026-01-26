72dcf4d36476313428c50483f965f55a02078f91
Eswar <eswar@company.com>
2026-01-26 15:21:57 -0500

    Add remote backend configuration using Azure storage account

--- backend.tf
+++ backend.tf
@@ -4,12 +4,5 @@ terraform {
     storage_account_name = "tfstatelzqiaypb"
     container_name       = "tfstate"
     key                  = "landingzone.tfstate"
-    # Retry settings , possible causes for retries include eventual consistency of storage account creation,
-    # GRS sometimes causes transient write failures during replication.
-    # Azure may not fully propagate DNS + replication for 30–60 seconds.
-    # A brief drop in your internet connection resets the PUT request.
-    # Terraform uses your CLI token for backend auth.
-    max_retries = 20
-    retry_delay = "5s"
   }
 }
